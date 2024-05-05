44a2b1d42130d7390fc09e46a633f9b7f489e213
JoKeRooo7 <renal98@mail.ru>
2024-05-05 15:40:06 +0300

    update neurons

--- src/neurons/structure_neuron.h
+++ src/neurons/structure_neuron.h
@@ -7,19 +7,19 @@
 
 
 #include "../../support_functions/saving_data.h"
+#include "../../support_functions/update_weight.h"
 #include "../../support_functions/activation_function.h"
 
 /* -----------==============================-------------
 This is a simple neuron model used to build 
 a perceptron.
 
+In the Neuron class:
+
+private fields:
 δ (error_) - the value of the error on the neuron
 o (output_value_) - the output value of the neuron
-η (learning_rate_) - learning rate
 w (weight) - weight between other neurons
-α (inertia_coefficient_) -  is the inertia 
-coefficient to smooth out sharp overshoots 
-as you move across the surface of the objective function.
 --------------==============================---------- */
 
 
@@ -27,29 +27,38 @@ namespace mlp {
 
 namespace graph {
 
-namespace abstract {
-
 // C - module For sending signals and copying data outside the neuron
 template <typename C>
 class Neuron {
     public:
+        // Neuron() = default;
+        explicit Neuron(std::size_t &neurol_id, std::size_t &layer_id, mlp::graph::WeightHandler *ready_module);
+        ~Neuron() = default;
 
-    protected:
+
+        void CalculateError();
+
+    // protected:
 
     private:
         float error_;  // δ
         float output_value_;  // o
-        float learning_rate_;  // η
-        
+
+
         std::size_t layer_id_;
         std::size_t neuron_id_;
-        std::vector<std::pair<float> prev_w_children_;  // w
-        std::vector<std::pair<float> error_prev_weight_;  // Δw
-        std::vector<std::pair<float*, Neuron<C>*>> parent_;  // w
-        std::vector<std::pair<float*, Neuron<C>*>> children_;  // w
+        // weights at the previous update
+        std::vector<std::pair<float> prev_weight_children_;
+        // the error of the scales on the previous update of the scales
+        std::vector<std::pair<float> error_prev_weight_;
+        // the weight between this neuron and the parent + parent
+        std::vector<std::pair<float*, Neuron<C>*>> parent_;
+        // the weight between this neuron and the child + child
+        std::vector<std::pair<float, Neuron<C>*>> children_;
+
+        mlp::graph::WeightHandler *module_weith_handler;
 };
 
-}  // abstract
 
 }  // graph
 
