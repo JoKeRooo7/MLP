5ee140d1b04453a4bf1804479054e82d80c57d65
JoKeRooo7 <renal98@mail.ru>
2024-11-27 19:46:39 +0300

    архитекутра

---
+++ src/perceptron/edge/edge.h
@@ -0,0 +1,37 @@
+#ifndef MLP_PERCEPTRON_EDGE_EDGE_H
+#define MLP_PERCEPTRON_EDGE_EDGE_H
+
+
+#include "../neurons/template_neuron.h"
+
+
+namespace mlp {
+
+    template <typename W>
+    class Edge {
+        public:
+            using Weight = W;
+
+            Edge() = default;
+            Edge(Neuron *left_neuron);
+            Edge(Neuron *left_neuron, Neuron *right_neuron);
+            void AddLeftNeuron(Neuron *left_neuron);
+            void AddRightNeuron(Neufon *right_neuron);
+            // TODO - add UPDATE WEIGHT
+            // TODO - add calculcate error
+            // TODO -  add lear
+            ~Edge();
+        
+        private:
+            Neuron *left_neuron_ = nullptr;
+            Neuron *right_neuron_ = nullptr;
+            Weight this_weight_;
+
+            void ResetWeight();
+    };
+
+
+}  // mlp
+
+
+#endif  // MLP_PERCEPTRON_EDGE_EDGE_H
