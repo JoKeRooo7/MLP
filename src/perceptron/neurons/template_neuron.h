b37ae069bb39ee5f92e2f684172c83fc40d119c5
JoKeRooo7 <renal98@mail.ru>
2024-11-28 18:06:12 +0300

    upd

--- src/perceptron/neurons/template_neuron.h
+++ src/perceptron/neurons/template_neuron.h
@@ -2,8 +2,6 @@
 #define MLP_PERCEPTRON_NEURONS_TEMPLATE_NEURON_H_
 
 
-#include <cstddef>  // size_t
-#include <vector>
 
 
 namespace mlp {
@@ -11,13 +9,10 @@ namespace mlp {
     class Neuron {
         public:
 
-        private:
-            std::size_t id;
-            std::size_t layer_id;
-            float output_{0,0};
-            
+
     };  // Neuron
 
 }  // mlp
+
 
 #endif  //  MLP_PERCEPTRON_NEURONS_TEMPLATE_NEURON_H_
